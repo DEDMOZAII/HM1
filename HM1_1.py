8dd3277cffd19cabf58ed1a577cae47363cc7cd1
DEDMOZAII <GentelmanZocker@Gmail.com>
2024-09-18 19:10:51 +0300

    Create HM1_1.py

---
+++ HM1_1.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+
+def get_days_from_today(date):
+    try:
+        date_str = datetime.strptime(date, "%Y-%m-%d").date()
+        current_date = datetime.today().date()
+        difference_date = current_date - date_str
+        return difference_date
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'."
+print(get_days_from_today("2020-10-13"))
